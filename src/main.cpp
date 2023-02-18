1aba156e802cc164545920a948b5b22baff83a32
cameron <hammerbro_97@hotmail.com>
2023-02-18 15:21:36 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#define SDL_MAIN_HANDLED
+
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "SDL works" <<std::endl;
+    std::cin.get();
+
+    return 0;
+}
